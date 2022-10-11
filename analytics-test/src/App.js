884ec507de1404d1f7ab65ac533cb841ce6045ff
Adam Iverson <aciverso@gmail.com>
2022-10-11 11:48:45 -0500

    toml

--- analytics-test/src/App.js
+++ analytics-test/src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
+    <Switch>
+      </Switch>
   );
 }
 
