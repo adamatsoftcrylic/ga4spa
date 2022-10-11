0e11f6dca6003ccafa5441a5a01c76ec1c6379a0
Adam Iverson <aciverso@gmail.com>
2022-10-11 16:52:23 -0500

    form init

--- analytics-test/src/paths/Home.jsx
+++ analytics-test/src/paths/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Form from './Form';
+
 export default function Home() {
   return (
     <div>
       <h1>Home</h1>
       <Link className="link" to="/about">About</Link>
+      <Form />
     </div>
   )
 }
