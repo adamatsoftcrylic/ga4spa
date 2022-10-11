bff0bef6906b19af6ce42e94fbeff9dba2da12ae
Adam Iverson <aciverso@gmail.com>
2022-10-11 16:39:32 -0500

    some very nice time styling

--- analytics-test/src/paths/About.jsx
+++ analytics-test/src/paths/About.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export default function About() {
   return (
     <div>
       <h1>About</h1>
-      <Link to="/">Home</Link>
+      <NavLink className="link" to="/">Home</NavLink>
     </div>
   )
 }
