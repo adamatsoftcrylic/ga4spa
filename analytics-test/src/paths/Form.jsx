13da6a4e1c7a90ae952eb3d4caa1794473820ccf
Adam Iverson <aciverso@gmail.com>
2022-10-11 20:05:19 -0500

    form name

--- analytics-test/src/paths/Form.jsx
+++ analytics-test/src/paths/Form.jsx
@@ -1,20 +1,30 @@
-import React from "react";
+import { React, useState } from "react";
 
 export default function Form() {
+  let [name, setName] = useState("")
+
+  const logTown = (e) => {
+    e.preventDefault();
+    console.log(name);
+  }
+
   return (
     <form>
       <ul>
         <li>
-          <label for="name">Name:</label>
-          <input type="text" id="name" name="user_name" />
+          <label htmlFor="name">Name:</label>
+          <input type="text" id="name" name="user_name"  onChange={(e) => setName(e.target.value)} />
         </li>
         <li>
-          <label for="mail">E-mail:</label>
+          <label htmlFor="email">E-mail:</label>
           <input type="email" id="mail" name="user_email" />
         </li>
         <li>
-          <label for="msg">Message:</label>
+          <label htmlFor="msg">Message:</label>
           <textarea id="msg" name="user_message"></textarea>
+        </li>
+        <li>
+          <button type="submit" onClick={logTown}>Submit</button>
         </li>
       </ul>
     </form>
