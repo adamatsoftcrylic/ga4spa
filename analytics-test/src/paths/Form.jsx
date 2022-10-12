c5ff42e1f37e6801a12f8530236cb7773ca14d3d
Adam Iverson <aciverso@gmail.com>
2022-10-11 19:01:29 -0500

    form

--- analytics-test/src/paths/Form.jsx
+++ analytics-test/src/paths/Form.jsx
@@ -2,8 +2,21 @@ import React from "react";
 
 export default function Form() {
   return (
-    <Form>
-      <input>TEST INPUT</input>
-    </Form>
+    <form>
+      <ul>
+        <li>
+          <label for="name">Name:</label>
+          <input type="text" id="name" name="user_name" />
+        </li>
+        <li>
+          <label for="mail">E-mail:</label>
+          <input type="email" id="mail" name="user_email" />
+        </li>
+        <li>
+          <label for="msg">Message:</label>
+          <textarea id="msg" name="user_message"></textarea>
+        </li>
+      </ul>
+    </form>
   )
 }
