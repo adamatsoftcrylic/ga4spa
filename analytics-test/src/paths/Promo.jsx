a902a8a63216fa10bbe7da2840ad2d290203193e
Adam Iverson <Adam-Iverson@Adams-MacBook.local>
2022-10-14 12:12:53 -0500

    teest

--- analytics-test/src/paths/Promo.jsx
+++ analytics-test/src/paths/Promo.jsx
@@ -1,6 +1,14 @@
-import { React } from "react";
+import { React, useEffect } from "react";
+
+
 
 export default function Promo() {
+  useEffect(() => {
+    dataLayer.push({
+      'event': "test"
+    })
+  }, []);
+
   return (
     <div id="promo">Promo Box</div>
   )
